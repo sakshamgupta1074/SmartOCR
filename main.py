1423a2221c3de0fb2b6ad03519d2c0a7cb507649
Saksham <sakshamgupta1074@gmail.com>
2021-02-03 22:20:53 +0530

    main changed

--- main.py
+++ main.py
@@ -13,6 +13,55 @@ from xlutils.copy import copy
 from xlrd import open_workbook
 import xlsxwriter
 import detect3
+import cv2
+import numpy as np
+
+def preproces_image(
+	image,
+	*,
+	kernel_size=15,
+	crop_side=50,
+	blocksize=35,
+	constant=15,
+	max_value=255,
+):
+	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	bit = cv2.bitwise_not(gray)
+	image_adapted = cv2.adaptiveThreshold(
+		src=bit,
+		maxValue=max_value,
+		adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C,
+		thresholdType=cv2.THRESH_BINARY,
+		blockSize=blocksize,
+		C=constant,
+	)
+	kernel = np.ones((kernel_size, kernel_size), np.uint8)
+	erosion = cv2.erode(image_adapted, kernel, iterations=2)
+	return erosion[crop_side:-crop_side, crop_side:-crop_side]
+
+def find_edges(image_preprocessed, *, bw_threshold=150, limits=(0.09, 0.08)):
+	mask = image_preprocessed < bw_threshold
+	edges = []
+	for axis in (1, 0):
+		count = mask.sum(axis=axis)
+		limit = limits[axis] * image_preprocessed.shape[axis]
+		index_ = np.where(count >= limit)
+		_min, _max = index_[0][0], index_[0][-1]
+		edges.append((_min, _max))
+	return edges
+
+
+def adapt_edges(edges, *, height, width):
+	(x_min, x_max), (y_min, y_max) = edges
+	x_min2 = x_min
+	x_max2 = x_max + min(250, (height - x_max) * 10 // 11)
+	# could do with less magic numbers
+	y_min2 = max(0, y_min)
+	y_max2 = y_max + min(250, (width - y_max) * 10 // 11)
+	return (x_min2, x_max2), (y_min2, y_max2)
+
+
+
 def ocr(image_path):
 	os.environ['COMPUTER_VISION_SUBSCRIPTION_KEY'] = 'b080de7cbdda4fd6817e83e3fdf6706f'
 	os.environ['COMPUTER_VISION_ENDPOINT'] = 'https://imagetotextey.cognitiveservices.azure.com/'
@@ -153,6 +202,17 @@ def alltoexcel(mainfilename):
 				count=count+1
 				newfilename = head + str(i + 1) + '.jpg'
 				Image(image).save(filename=newfilename)
+				filename_in = newfilename
+				filename_out = newfilename
+				image = cv2.imread(str(filename_in))
+				height, width = image.shape[0:2]
+				image_preprocessed = preproces_image(image)
+				edges = find_edges(image_preprocessed)
+				(x_min, x_max), (y_min, y_max) = adapt_edges(
+					edges, height=height, width=width
+				)
+				image_cropped = image[x_min:x_max, y_min:y_max]
+				cv2.imwrite(str(filename_out), image_cropped)
 				detect3.detectshapescs2(newfilename,"Org_Chart-"+str(count),workbook)
 			workbook.close()
 		return newfilename
@@ -165,6 +225,17 @@ def alltoexcel(mainfilename):
 				count=count+1
 				newfilename = head + str(i + 1) + '.jpg'
 				Image(image).save(filename=newfilename)
+				filename_in = newfilename
+				filename_out = newfilename
+				image = cv2.imread(str(filename_in))
+				height, width = image.shape[0:2]
+				image_preprocessed = preproces_image(image)
+				edges = find_edges(image_preprocessed)
+				(x_min, x_max), (y_min, y_max) = adapt_edges(
+					edges, height=height, width=width
+				)
+				image_cropped = image[x_min:x_max, y_min:y_max]
+				cv2.imwrite(str(filename_out), image_cropped)
 				detect3.detectshapescs2(newfilename,"Org_Chart-"+str(count),workbook)
 			workbook.close()
 		return newfilename
@@ -177,6 +248,17 @@ def alltoexcel(mainfilename):
 				count=count+1
 				newfilename = head + str(i + 1) + '.jpg'
 				Image(image).save(filename=newfilename)
+				filename_in = newfilename
+				filename_out = newfilename
+				image = cv2.imread(str(filename_in))
+				height, width = image.shape[0:2]
+				image_preprocessed = preproces_image(image)
+				edges = find_edges(image_preprocessed)
+				(x_min, x_max), (y_min, y_max) = adapt_edges(
+					edges, height=height, width=width
+				)
+				image_cropped = image[x_min:x_max, y_min:y_max]
+				cv2.imwrite(str(filename_out), image_cropped)
 				detect3.detectshapescs2(newfilename,"Org_Chart-"+str(count),workbook)
 			workbook.close()
 		return newfilename
@@ -189,6 +271,17 @@ def alltoexcel(mainfilename):
 				count=count+1
 				newfilename = head + str(i + 1) + '.jpg'
 				Image(image).save(filename=newfilename)
+				filename_in = newfilename
+				filename_out = newfilename
+				image = cv2.imread(str(filename_in))
+				height, width = image.shape[0:2]
+				image_preprocessed = preproces_image(image)
+				edges = find_edges(image_preprocessed)
+				(x_min, x_max), (y_min, y_max) = adapt_edges(
+					edges, height=height, width=width
+				)
+				image_cropped = image[x_min:x_max, y_min:y_max]
+				cv2.imwrite(str(filename_out), image_cropped)
 				detect3.detectshapescs2(newfilename,"Org_Chart-"+str(count),workbook)
 			workbook.close()
 		return newfilename
@@ -295,4 +388,4 @@ def convertndocr(doc_path):
 def deletedir():
 	files = glob.glob('/tempwand/*')
 	for f in files:
-	    os.remove(f)
+		os.remove(f)
